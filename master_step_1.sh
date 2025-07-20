9eab2ba1cf5f3ea544eecb4e27365407040c311d
Nikhil Patwardhan <nikhilpatwardhan@Nikhils-iMac.local>
2025-07-19 22:24:50 -0700

    fix script

--- master_step_1.sh
+++ master_step_1.sh
@@ -1,55 +1,10 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
+if [ "$EUID" -ne 0 ]
+then
   echo "Please run as root or with sudo"
   exit 1
 fi
-
-# These instructions are to create the master node on a virtual machine in Truenas Scale (Fangtooth)
-
-# Steps to be performed before running this script:
-# Create a ZVol of size 10 GiB that will act as the disk on which we will install Ubuntu Server 24.04
-# Download the ISO image from the internet and upload it to Truenas
- 
-# Create a new instance with these parameters through the GUI
-# -----------------------------------------------------------
-# 2 CPU
-# 8 GiB RAM
-# 6 GiB Root Disk
-# Attached ZVol onto which the OS will be installed
-# Select the appropriate NIC
-# Setup a VNC port of 5901 (so that it does not clash with 5900 already in use)
-# 
-# Use a VNC viewer (Screen Sharing app on iMac) to login to the machine
-# and begin the Ubuntu installation process
-# Choose the minimized image
-# I did not choose OpenSSH server from the GUI installation steps, but maybe I should.
-# Once installation is complete, and reboot is done, login to the machine through the VNC client
-# Run the command
-# ip a
-# This will show the MAC address of the interface
-# Create a DHCP reservation in the router to assign the IP address 10.12.1.12 to this MAC address
-# Restart both the router and the VM, this should now take effect. Verify by running ip a again.
-
-# Verify that swap space is off
-# nikhil@k8smaster:~$ free -m
-#                total        used        free      shared  buff/cache   available
-# Mem:            7929         513        6905           4         826        7416
-# Swap:              0           0           0
-
-# Initially, ipv4 forwarding is off
-# nikhil@k8smaster:~$ sysctl net.ipv4.ip_forward
-# net.ipv4.ip_forward = 0
-
-# nikhil@k8smaster:~$ cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-# > net.ipv4.ip_forward = 1
-# > EOF
-# net.ipv4.ip_forward = 1
-# sudo sysctl --system
-
-# Check which cgroup driver is being used on the system
-# nikhil@k8smaster:~$ stat -fc %T /sys/fs/cgroup/
-# cgroup2fs
 
 
 # Overall the steps we are following are those documented in
